3002db147271d3413de10663489c79f48644f6f5
jzx <945627077@qq.com>
2022-05-23 17:58:15 +0800

    重构

--- site/components/SearchInput.vue
+++ site/components/SearchInput.vue
@@ -2,7 +2,7 @@
   <div
     ref="searchForm"
     v-click-outside="onBlur"
-    class="searchFormDiv"
+    class=" border rounded-full relative "
     :class="{ 'input-focus': inputFocus, 'show-histories': showHistories }"
   >
     <div class="search-input">
@@ -25,7 +25,7 @@
         <i class="iconfont icon-search" />
       </span>
     </div>
-    <div class="histories">
+    <div class="histories absolute">
       <ul>
         <li
           v-for="(item, index) in histories"
@@ -171,128 +171,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.searchFormDiv {
-  @media screen and (max-width: 768px) {
-    & {
-      display: none;
-    }
-  }
 
-  $search-box-width: 380px;
-  $border-color: #4e6ef2; // TODO
-
-  &.input-focus {
-    .search-input {
-      background-color: var(--bg-color);
-      border: 1px solid var(--border-color3);
-    }
-    .icon-search {
-      color: #0065ff;
-    }
-  }
-
-  &.show-histories {
-    .histories {
-      display: block;
-    }
-  }
-
-  .search-input {
-    width: $search-box-width;
-    background-color: var(--bg-color2);
-    border: 1px solid var(--border-color);
-    height: 34px;
-    border-radius: 17px;
-    padding: 4px 10px;
-    font-size: 14px;
-    display: flex;
-    transition-property: background-color, border, color;
-    transition-duration: 0.25s;
-    transition-timing-function: ease-in;
-
-    input {
-      color: var(--text-color);
-      font-weight: 400;
-      font-size: 13px;
-      height: 24px;
-      line-height: 24px;
-      flex: 1 1;
-      padding: 0;
-      margin: 0 5px;
-      overflow: hidden;
-      background: transparent;
-      border: none;
-      resize: none;
-      box-shadow: none;
-
-      &:focus {
-        outline: none;
-      }
-    }
-  }
-
-  .histories {
-    width: $search-box-width;
-    display: none;
-    height: auto;
-    top: 48px;
-    position: fixed;
-
-    background-color: var(--bg-color);
-    border: 1px solid var(--border-color);
-    border-radius: 4px;
-    box-shadow: 0 5px 20px rgb(18 18 18 / 10%);
-    z-index: 203;
-
-    animation: showHistoriesAnimation 0.3s;
-    -moz-animation: showHistoriesAnimation 0.3s; /* Firefox */
-    -webkit-animation: showHistoriesAnimation 0.3s; /* Safari and Chrome */
-    -o-animation: showHistoriesAnimation 0.3s; /* Opera */
-
-    @keyframes showHistoriesAnimation {
-      0% {
-        opacity: 0;
-        transform: translate(1px, 30px);
-      }
-      100% {
-        opacity: 1;
-        transform: translate(0, 0);
-      }
-    }
-    ul {
-      li {
-        padding: 7px 16px;
-        width: 100%;
-        text-align: left;
-        cursor: pointer;
-        -webkit-box-sizing: border-box;
-        box-sizing: border-box;
-
-        display: flex;
-        span {
-          width: 100%;
-          font-size: 13px;
-          font-weight: 400;
-          color: var(--text-color);
-        }
-        &:hover {
-          i.iconfont {
-            display: block;
-          }
-        }
-        i.iconfont {
-          display: none;
-          font-size: 13px;
-          padding: 0 6px;
-          &:hover {
-            color: #4e6ef2;
-          }
-        }
-        &.selected {
-          background-color: var(--bg-color2);
-        }
-      }
-    }
-  }
-}
 </style>
